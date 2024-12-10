7add4eec66974ac0a3c4a8160ccd5027a39dd85d
avgKol <anantavijayguha@gmail.com>
2024-12-09 21:18:14 -0500

    pcc_1

---
+++ chapter_02/exercise.py
@@ -0,0 +1,32 @@
+message ="Welcome to New Jersey"
+print(message)
+message = "Welcome to New York"
+print(message)
+name= "The Jackal"
+
+print (f"Hello {name}, would you like to learn some Python today?")
+print (f"Hello {name.lower()}, would you like to learn some Python today?")
+print (f"Hello {name.upper()}, would you like to learn some Python today?")
+print (f"Hello {name.title()}, would you like to learn some Python today?")
+
+# print (f"hello {name.translate()} , is everything ok? ")
+
+famous_person = "Albert Einstein"
+quote = "The only source of knowledge is experience."
+print (f"{famous_person} once said, '{quote}'")
+print (5+3)
+print (10-2)
+print (4*2)
+print (16/2)
+
+favorite_number = 8
+message = f"My favorite number is {favorite_number}"
+print(message)
+
+name = "\t\n  Jackal  \n\t"
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+
+file_name= 'python_notes.txt'
+print(file_name.removesuffix('.txt'))

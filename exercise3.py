1431b85ed1e803f8f3f453ecbeeae1269cc4a6f3
avgKol <anantavijayguha@gmail.com>
2024-12-25 20:07:48 -0500

    exercise

--- exercise3.py
+++ exercise3.py
@@ -1,5 +1,7 @@
+from turtle import color
 import typing
 import collections
+
 
 fruit_tuple = ("apple", "banana", "pear", "berry", "coconut")
 print(type(tuple))
@@ -31,3 +33,48 @@ print(player3, score3)
 
 for player, score in nested_tuples:
     print(player, score)
+
+language_tuples = ("java", "python", "go", "c", "pascal")
+for lang in language_tuples:
+    print(f"language is {lang}")
+
+dup_tuples = (1, 2, 3, 2, 1, 4, 5, 2)
+print(f"==>> dup_tuples.count(2): {dup_tuples.count(2)}")
+print(f"==>> dup_tuples.index(4): {dup_tuples.index(4)}")
+
+countries_tuple = ("USA", "India", "France", "Germany")
+if "Canada" in countries_tuple:
+    print(f'==>> "yes": {"yes"}')
+else:
+    print(f'==>> "No": {"No"}')
+
+
+integer_tuple = (10, 20, 30, 40, 50)
+integer_list = list(integer_tuple)
+integer_list.append(60)
+new_integer_tuple = tuple(integer_list)
+print(f"==>> new_integer_tuple: {new_integer_tuple}")
+
+# for number in 1 to 10
+squares_tuple = tuple(x**2 for x in range(1, 11))
+print(f"==>> squares_tuple: {squares_tuple}")
+
+fruit_list = ["banana", "apple", "pear", "coconut", "papaya"]
+print(f"==>> fruit_list[1]: {fruit_list[1]}")
+print(f"==>> fruit_list[3]: {fruit_list[3]}")
+print(f"==>> fruit_list[-1]: {fruit_list[-1]}")
+
+numbers_list = list(x*10 for x in range(0, 10, 1) if True )
+print(f"==>> numbers_list: {numbers_list}")
+numbers_list[2]=100
+numbers_list.append(60)
+numbers_list.remove(40)
+print(f"==>> numbers_list: {numbers_list}")
+
+colors_list = ["red", "blue", "green", "yellow", "purple"]
+print(f"==>> colors_list[1:3]: {colors_list[1:3]}")
+for index in range(0,5):
+    print(colors_list[:len(colors_list)-index][::-1])
+
+    
+    
